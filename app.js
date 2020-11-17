ee1873782d4275395d73d8dbc66cd240f25d0955
clarafonseca <clarafonseca@users.noreply.github.com>
2020-11-17 00:58:34 -0300

    adicionando pesquisa e requisicao ajax

--- app.js
+++ app.js
@@ -1,26 +1,56 @@
-const TMDB_ENDPOINT_BASE = 'https://api.themoviedb.org/3'
-//exercutar a requisição AJAX
-function MostraFilmesEmCartaz() {
-    &.ajax({
-        //Passar a URL ENDPOINT (method: get, default)
-        url: TMDB_ENDPOINT_BASE + '/movie/',
-        data: {
-            api_key: '97fb9e05c6c8ada9e05d5be0994c5389'
-        }
-    })
-    // Receber o JSON
-    .done(function (data){
-        let codigo_html = '';
-        for (i=0; i< xxxxx; i++){
-            //Concatenar o código do card com os dados do JSON
-            codigo_html += '';
-        }
-        $('lista_filmes').html(codigo_html)
-    });
+const TMDB_ENDPOINT_BASE = "https://api.themoviedb.org/3";
+
+function mudarCard(data) {
+  let codigo_html = "";
+
+  for (i = 0; i < data.results.length; i++) {
+    titulo = data.results[i].title;
+    imagem = "https://image.tmdb.org/t/p/w500" + data.results[i].poster_path;
+    estreia = data.results[i].release_date;
+
+    if (data.results[i].poster_path != null) {
+      codigo_html += `<div class="card">
+      <img src="${imagem}" alt="${titulo}" class="card-img">
+      <div class="card-body">
+          <h4 class="card-title">${titulo}</h5>
+          <p class="card-text">
+              ${estreia}
+          </p>
+          <a href="#" class="card-btn">Mais info</a>
+      </div>
+</div>`;
+
+      $("#lista_filmes").html(codigo_html);
+    }
+  }
 }
 
-$(document).ready(function(){
-    alert('Bem vindo
-    ')
-    MostraFilmesEmCartaz();
+//exercutar a requisição AJAX
+function MostraFilmesEmCartaz() {
+  $.ajax({
+    //Passar a URL ENDPOINT (method: get, default)
+    url: TMDB_ENDPOINT_BASE + "/movie/popular",
+    data: {
+      api_key: "97fb9e05c6c8ada9e05d5be0994c5389",
+    },
+  })
+    .done(mudarCard);
+}
+
+function pesquisaFilmes() {
+  valorPesquisado = search.value;
+  payload = {
+    api_key: "97fb9e05c6c8ada9e05d5be0994c5389",
+    query: valorPesquisado,
+  };
+  $.ajax({
+    //Passar a URL ENDPOINT (method: get, default)
+    url: TMDB_ENDPOINT_BASE + "/search/movie",
+    data: payload,
+  }).done(mudarCard);
+}
+
+$(document).ready(function () {
+  MostraFilmesEmCartaz();
+  $("#btn_pesquisar").click(pesquisaFilmes);
 });
