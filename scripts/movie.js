e9e9db3340bf5a21fa9ab04184793cf4ed1b04ca
Ana Clara Fonseca <claracamposbh@hotmail.com>
2020-12-05 17:16:49 -0300

    .

--- scripts/movie.js
+++ scripts/movie.js
@@ -70,7 +70,7 @@ $(document).ready(function () {
   MostraFilme();
   $(".search-box").submit(function (e) {
     let pesquisa = $(".search-text").val();
-    window.location.href = `/search.html?query=${pesquisa}`;
+    window.location.href = `filmart-v2/pages/search.html?query=${pesquisa}`;
     e.preventDefault();
   });
 });
